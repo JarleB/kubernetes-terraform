23162f81eaeebb4d55b7c9c6294aef5633628bab
Jarle Bjørgeengen <jarle.bjorgeengen@usit.uio.no>
2017-05-10 14:21:26 +0200

    Initial support for uh-iaas

---
+++ run_uh-iaas.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+tf_dir="terraform-uh-iaas"
+if [ ! -f ${tf_dir}/local.tfvars ]; then
+    echo "You must create terraform/local.tfvars" >&2
+    exit 1
+fi
+
+pushd ${tf_dir}
+# This first (serial) run of the security group rules are needed due to strange
+# behaviour where only some of the rules are there after the initial run unless
+# the are processed sequentially.
+#
+# See:
+#     https://github.com/hashicorp/terraform/issues/7519
+#
+#terraform apply -var-file ipnett.tfvars -var-file local.tfvars -parallelism 1 \
+#          -target openstack_networking_secgroup_rule_v2.ssh_access_ipv4 \
+#          -target openstack_networking_secgroup_rule_v2.kube_lb_http_ipv4 \
+#          -target openstack_networking_secgroup_rule_v2.kube_lb_https_ipv4 \
+#          -target openstack_networking_secgroup_rule_v2.kube_master_ipv4
+
+# Now, do the rest in parallell as normal
+terraform apply -var-file uh-iaas.tfvars -var-file local.tfvars 
+terraform output inventory >../ansible/inventory/hosts
+popd
+
+./ansible/apply.sh
